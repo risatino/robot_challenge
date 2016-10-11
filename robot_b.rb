6ec7c3442d18fe8366e2dfef429b4fabfb0acbc1
risatino <charissatolentino@gmail.com>
2016-10-11 11:10:38 -0500

    The stock robot_b exercise now works! changed bid_realtime to .bid

--- robot_b.rb
+++ robot_b.rb
@@ -5,12 +5,12 @@ require 'stock_quote'
 
 class RobotB
   def get_stock_price(symbol)
-    `say "The price of #{symbol} is #{StockQuote::Stock.quote(symbol).bid} dollars"`
+    `say "The price of #{symbol} is #{StockQuote::Stock.quote("aapl").bid} dollars"`
   end
 end
 
 # solution
 robolady = RobotB.new
-robolady = robolady.get_stock_price("symbol")
+robolady = robolady.get_stock_price("aapl")
 
 # I couldn't install the stock_quote gem for some reason, but I assume if I were able to, it would look something like what I wrote above.
