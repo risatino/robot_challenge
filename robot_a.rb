6acae8f296ecbfb8ca0c0b2924ceb22570b48256
risatino <charissatolentino@gmail.com>
2016-11-01 23:00:12 -0500

    updates to robot app

--- robot_a.rb
+++ robot_a.rb
@@ -2,10 +2,19 @@
 
 class RobotA
   def dry_my_hands
-    `say ooooooooooooooooooooooooooooooooooooooooooooooooooooo`
+    `say ooooooooooooooooooooooooooooooooooooooooo ooooo oooooooo`
   end
 end
 
 # solution
 robolad = RobotA.new
 robolad.dry_my_hands
+
+# class Cub
+#   def to_annoy_people
+#     `say yaaaaaaaaaaaaaahooooooooooo oooowee oooooweee cha cha cha cubbees`
+#   end
+# end
+
+# didsomething = Cub.new
+# didsomething.to_annoy_people
